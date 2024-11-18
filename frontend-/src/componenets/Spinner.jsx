3c71cdcebd9badcf7c3255e8d14e3ec7a565969c
AfrozSheikh <sa8329925@gmail.com>
2024-11-18 09:33:13 +0530

    loader Added

--- frontend-/src/componenets/Spinner.jsx
+++ frontend-/src/componenets/Spinner.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import "./Spinner.css"
+import React from 'react';
+import "./Spinner.css";
 
 const Spinner = () => {
   return (
-    <div>
-      <div className='spinner'></div>
+    /* From Uiverse.io by NlghtM4re */
+    <div className="container">
+      <div className="tree">
+        <div className="branch" style={{ "--x": 0 }}>
+          <span style={{ "--i": 0 }}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
+        </div>
+        <div className="branch" style={{ "--x": 1 }}>
+          <span style={{ "--i": 0 }}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
+        </div>
+        <div className="branch" style={{ "--x": 2 }}>
+          <span style={{ "--i": 0 }}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
+        </div>
+        <div className="branch" style={{ "--x": 3 }}>
+          <span style={{ "--i": 0 }}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
+        </div>
+        <div className="stem">
+          <span style={{ "--i": 0 }}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
+        </div>
+        <span className="shadow"></span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Spinner
+export default Spinner;
